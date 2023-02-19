7489a9c05eee637c7de18435c4eb15bb8d225e1d
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-02-18 18:20:17 -0800

    Time: 56 ms (78.17%), Space: 14.2 MB (94.54%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -0,0 +1,9 @@
+
+
+class Solution:
+    def sortArrayByParity(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        return sorted(A,key = lambda x:x%2)
