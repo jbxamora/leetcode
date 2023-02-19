ff767d9e8dc9c0d9ceb0f2057a7391b08496bc2f
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-02-18 19:12:17 -0800

    Time: 22 ms (98.27%), Space: 13.6 MB (38.45%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,49 +1,32 @@
 # Problem statement: Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and appear as many times as it shows in both arrays.
 
-# One approach to solve this problem is to sort both arrays nums1 and nums2, and then use two pointers to traverse them. We can use i to traverse nums1 and j to traverse nums2. If nums1[i] is greater than nums2[j], we increment j. If nums1[i] is less than nums2[j], we increment i. If nums1[i] is equal to nums2[j], then we add nums1[i] to the result array res if it is not already present in res, and we increment both i and j. We repeat this process until one of the arrays is fully traversed.
+# Another approach to solve this problem is to use sets. We can convert both arrays nums1 and nums2 to sets, and then use the set intersection operator & to find the common elements between the two sets. Finally, we convert the resulting set back to a list and return it.
 
 # The steps to get the solution of this problem are as follows:
 
 # Define a function intersection(nums1, nums2) that takes two integer arrays nums1 and nums2 as input.
 
-# Sort both arrays nums1 and nums2 using the sort() method.
+# Convert nums1 and nums2 to sets using the set() function.
 
-# Initialize two pointers i and j to 0.
+# Find the intersection of the two sets using the set intersection operator &.
 
-# Use a while loop to traverse both arrays. In each iteration of the loop, check if nums1[i] is greater than nums2[j]. If it is, increment j. If nums1[i] is less than nums2[j], increment i. If nums1[i] is equal to nums2[j], add nums1[i] to the result array res if it is not already present in res, and increment both i and j.
+# Convert the resulting set back to a list using the list() function.
 
-# Repeat steps 4 until one of the arrays is fully traversed.
+# Return the list of common elements.
 
-# Return the result array res.
-
-# The time complexity of this approach is O(n log n), where n is the length of the longer input array after sorting. The space complexity is O(n) to store the result array.
+# The time complexity of this approach is O(n), where n is the total number of elements in both input arrays. The space complexity is also O(n) to store the sets.
 
 # Here are some additional notes on the approach:
 
-# The sort() method sorts an array in ascending order. In the provided solution, we sort both nums1 and nums2 in ascending order before traversing them.
+# The set() function creates a set from a given iterable (such as a list or a tuple). Sets are unordered collections of unique elements.
 
-# The result array res is initialized as an empty list at the beginning of the function.
+# The set intersection operator & returns a set containing the common elements between two sets.
 
-# The check if nums1[i] not in res: is used to ensure that each element in the result is unique. If nums1[i] is already present in res, we do not add it again.
+# The list() function creates a list from a given iterable. In the provided solution, we use this function to convert the resulting set back to a list.
 
-# Using two pointers to traverse two arrays is a common approach to solving array problems. It has a time complexity of O(n) and is useful for finding patterns in two sorted arrays or searching for elements in unsorted arrays.
+# Using sets is a useful approach when we need to find the intersection or union of two lists and we do not care about the order of the elements. However, sets are not useful when we need to preserve the order of the elements or when we need to access elements by index.
 
 class Solution(object):
     def intersection(self, nums1, nums2):
-        res = []
-        nums1.sort()
-        nums2.sort()
-        i = j = 0
+        return list(set(nums1) & set(nums2))
         
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] > nums2[j]:
-                j += 1
-            elif nums1[i] < nums2[j]:
-                i += 1
-            else:
-                if nums1[i] not in res:
-                    res.append(nums1[i])
-                i += 1
-                j += 1
-        
-        return res
