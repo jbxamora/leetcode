f38cae80599832819bae61cdb686eee70cb43b69
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-02-20 02:14:35 -0800

    Time: 144 ms (48.22%), Space: 14.2 MB (62.19%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
@@ -1,7 +1,30 @@
-class Solution(object):
-     def hasGroupsSizeX(self, deck):
-        def gcd(a, b):
-            while b: a, b = b, a % b
-            return a
-        count = collections.Counter(deck).values()
-        return reduce(gcd, count) > 1
+class Solution:
+    def hasGroupsSizeX(self, deck):
+        # Create a dictionary that stores the count of each unique card
+        d = {}
+        for x in deck:
+            d[x] = d.get(x, 0) + 1
+        
+        # Compute the set of all card counts
+        res = set(d.values())
+        
+        # If there is a group with only one card, it is impossible to divide the cards into groups of X
+        if 1 in res:
+            return False
+        
+        # If all groups have the same number of cards, it is possible to divide the cards into groups of X
+        if len(res) == 1:
+            return True
+        
+        # Use the Euclidean algorithm to compute the greatest common divisor of the card counts
+        div = res.pop()
+        for x in res:
+            while x:
+                div, x = x, div % x
+        
+            # If the GCD is 1, it is impossible to divide the cards into groups of X
+            if div == 1:
+                return False
+        
+        # If the GCD is not 1, it is possible to divide the cards into groups of X
+        return div != 1
