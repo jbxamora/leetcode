2058ff2ab0e5fd05ea09fc0e5a7ed71c1912a012
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-02-20 01:54:01 -0800

    Time: 38 ms (35.84%), Space: 13.8 MB (94.63%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -1,20 +1,3 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        # Base case: if num is 1, it is a perfect square
-        if num == 1:
-            return True
-        
-        # Binary search the first half of the search space
-        left, right = 1, num // 2 + 1
-        while left <= right:
-            mid = (left + right) // 2
-            square = mid ** 2
-            if square == num:
-                return True
-            elif square < num:
-                left = mid + 1
-            else:
-                right = mid - 1
-        
-        # If the loop terminates without finding a perfect square, num is not a perfect square
-        return False
+        return int(num**0.5) == num**0.5
