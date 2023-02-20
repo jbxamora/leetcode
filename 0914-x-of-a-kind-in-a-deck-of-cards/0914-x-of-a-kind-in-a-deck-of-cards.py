f763c7724727adf5f41d8cbef72c2d992af700fa
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-02-20 02:20:40 -0800

    Time: 144 ms (48.22%), Space: 14.2 MB (94.66%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
@@ -1,34 +1,15 @@
-class Solution:
-    def hasGroupsSizeX(self, deck: List[int]) -> bool:
-        # Create a frequency dictionary to store the count of each unique card
-        freq = {}
+class Solution(object):
+    def hasGroupsSizeX(self, deck):
+        d = {}
         for x in deck:
-            if x in freq:
-                freq[x] += 1
-            else:
-                freq[x] = 1 
-        
-        # Get a list of the card counts, sorted in ascending order
-        values = sorted(freq.values())
-        
-        # Check if the smallest count is less than or equal to 1
-        smallest = values[0]
-        if smallest <= 1:
-            return False 
-        
-        # Use the square root of the smallest count as an upper bound for the possible values of X
-        m = math.ceil(smallest**(1/2))
-
-        # Check if the deck can be divided into groups of size X for X = 2, 3, ..., m+1
-        for i in range(2, m+2):
-            if smallest % i == 0:
-                if all(x % i == 0 for x in values[1:]):
-                    return True
-        
-        # Check if the deck can be divided into groups of size smallest.
-        if all(x % smallest == 0 for x in values[1:]):
-            return True
-        
-        # If the deck cannot be divided into groups of any size, return False.
-        return False
+            d[x] = d.get(x, 0) + 1
+        res = set(d.values())
+        if 1 in res: return False
+        if len(res) == 1: return True
+        div = res.pop()
+        for x in res:
+            while x:
+                div, x = x, div % x
+            if div == 1: return False
+        return div != 1
 
