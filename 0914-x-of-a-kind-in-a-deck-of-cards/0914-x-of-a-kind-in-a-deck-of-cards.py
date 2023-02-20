e00a4684c5016a8ee9667711a30dc6548d6eec4b
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-02-20 02:28:42 -0800

    Time: 147 ms (37.67%), Space: 14.2 MB (94.66%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
@@ -1,16 +1,4 @@
-from math import gcd
-from collections import Counter
-
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
-        count = Counter(deck)
-        values = list(count.values())
-        gcd_val = values.pop()
-
-        # Compute the GCD of all the counts in the deck
-        for val in values:
-            gcd_val = gcd(gcd_val, val)
-            if gcd_val == 1:
-                return False
-
-        return gcd_val >= 2
+        counter = collections.Counter(deck)
+        return math.gcd(*counter.values()) >= 2
