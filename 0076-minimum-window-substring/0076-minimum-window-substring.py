66d75a8716e36925241e9241723216d23c6530e6
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-02-20 01:45:24 -0800

    Time: 73 ms (99.97%), Space: 14.8 MB (29.39%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,44 +1,53 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # Initialize the count array for characters in t
-        count_t = [0] * 128
-        for char in t:
-            count_t[ord(char)] += 1
+        # Check if s is shorter than t, which means it cannot contain the minimum window
+        if len(s) < len(t):
+            return ""
 
-        # Initialize the left and right pointers, the required count, and the minimum window length and starting index
-        left, right = 0, 0
-        required_count = len(t)
-        min_window_len, min_window_start = float('inf'), 0
+        # Initialize a dictionary to store the frequency of characters in t
+        needstr = collections.defaultdict(int)
+        for ch in t:
+            needstr[ch] += 1
+
+        # Initialize the required count and the minimum window start and end indices
+        needcnt = len(t)
+        res = (0, float('inf'))
+
+        # Initialize the left pointer
+        start = 0
 
         # Loop through s using the right pointer
-        while right < len(s):
+        for end, ch in enumerate(s):
             # If the current character is required for t, decrement the required count
-            if count_t[ord(s[right])] > 0:
-                required_count -= 1
-            count_t[ord(s[right])] -= 1
+            if needstr[ch] > 0:
+                needcnt -= 1
+            needstr[ch] -= 1
 
             # While the current window contains all the characters in t, move the left pointer to the right
-            while required_count == 0:
-                # Update the minimum window if the current window is smaller
-                if right - left + 1 < min_window_len:
-                    min_window_len = right - left + 1
-                    min_window_start = left
+            if needcnt == 0:
+                while True:
+                    # Get the character at the left pointer
+                    tmp = s[start]
 
-                # If the character at the left pointer is required for t, increment the required count
-                count_t[ord(s[left])] += 1
-                if count_t[ord(s[left])] > 0:
-                    required_count += 1
+                    # If the character is not required for t, move the left pointer to the right
+                    if needstr[tmp] == 0:
+                        break
 
-                # Move the left pointer to the right
-                left += 1
+                    # If the character is required for t, increment its frequency and move the left pointer to the right
+                    needstr[tmp] += 1
+                    start += 1
 
-            # Move the right pointer to the right
-            right += 1
+                # Update the minimum window indices if the current window is smaller
+                if end - start < res[1] - res[0]:
+                    res = (start, end)
 
-        # Create the minimum window using string concatenation
-        min_window = "" if min_window_len == float('inf') else s[min_window_start:min_window_start + min_window_len]
+                # Increment the frequency of the character at the left pointer and the required count, and move the left pointer to the right
+                needstr[s[start]] += 1
+                needcnt += 1
+                start += 1
 
-        return min_window
+        # Create the minimum window using the minimum window indices
+        return '' if res[1] > len(s) else s[res[0]:res[1]+1]
 
 
 
