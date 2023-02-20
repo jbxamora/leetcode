dcd029e39ec5e680af542463b6741c0e4ef7f6f8
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-02-20 01:30:14 -0800

    Time: 139 ms (66.72%), Space: 14.6 MB (76.43%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,23 +1,23 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # Initialize the left and right pointers, the required count, and the minimum window length and starting index
-        left, right = 0, 0
-        required_count = len(t)
-        min_window_len, min_window_start = float('inf'), 0
-
         # Initialize the count array for characters in t
         count_t = [0] * 128
         for char in t:
             count_t[ord(char)] += 1
 
-        # Loop through s using the right pointer
+        # Initialize the left and right pointers, the required count, and the minimum window length and starting index
+        left, right = 0, 0
+        required_count = len(t)
+        min_window_len, min_window_start = float('inf'), 0
+
+        # Loop through s using both the left and right pointers
         while right < len(s):
             # If the current character is required for t, decrement the required count
             if count_t[ord(s[right])] > 0:
                 required_count -= 1
             count_t[ord(s[right])] -= 1
 
-            # While the current window contains all the characters in t, move the left pointer to the right
+            # If the current window contains all the characters in t, move the left pointer to the right and update the minimum window
             while required_count == 0:
                 # Update the minimum window if the current window is smaller
                 if right - left + 1 < min_window_len:
@@ -36,10 +36,11 @@ class Solution:
             right += 1
 
         # Create the minimum window using a list
-        if min_window_len != float('inf') and min_window_start + min_window_len <= len(s):
+        if min_window_len != float('inf'):
             min_window = [s[i] for i in range(min_window_start, min_window_start + min_window_len)]
             return ''.join(min_window)
         else:
             return ''
+
 
 
