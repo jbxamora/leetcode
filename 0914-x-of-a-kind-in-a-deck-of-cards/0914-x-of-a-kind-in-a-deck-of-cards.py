654b93ccbe9142a5571e0cc5c2ac9ad7f994f1b0
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-02-20 02:30:53 -0800

    Time: 145 ms (45.48%), Space: 14.3 MB (62.19%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
@@ -1,4 +1,15 @@
+import collections
+import math
+
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        # Count the frequencies of each card using a Counter object
         counter = collections.Counter(deck)
-        return math.gcd(*counter.values()) >= 2
+        
+        # Find the greatest common divisor of the counts
+        gcd_val = counter.popitem()[1]
+        for count in counter.values():
+            gcd_val = math.gcd(gcd_val, count)
+        
+        # Return True if the GCD is greater than or equal to 2, else False
+        return gcd_val >= 2
