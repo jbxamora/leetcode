f914f72ed01068e8672dc1d9a1609dad4d79c00a
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-02-20 02:26:26 -0800

    Time: 148 ms (35.48%), Space: 14.3 MB (62.19%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
@@ -1,33 +1,16 @@
-import math
+from math import gcd
+from collections import Counter
 
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
-        # Create a frequency dictionary to store the count of each unique card
-        freq = {}
-        for x in deck:
-            freq[x] = freq.get(x, 0) + 1
-        
-        # Get a list of the card counts, sorted in ascending order
-        values = sorted(freq.values())
-        
-        # Check if the smallest count is less than or equal to 1
-        smallest = values[0]
-        if smallest <= 1:
-            return False 
-        
-        # Find the factors of the smallest count.
-        factors = set()
-        for i in range(2, int(smallest ** 0.5) + 1):
-            if smallest % i == 0:
-                factors.add(i)
-                factors.add(smallest // i)
-        factors.add(smallest)
-        
-        # Check if the deck can be divided into groups of size X for any factor of the smallest count
-        for f in factors:
-            if all(x % f == 0 for x in values):
-                return True
-        
-        # If the deck cannot be divided into groups of any size, return False.
-        return False
+        count = Counter(deck)
+        values = list(count.values())
+        gcd_val = values.pop()
 
+        # Compute the GCD of all the counts in the deck
+        for val in values:
+            gcd_val = gcd(gcd_val, val)
+            if gcd_val == 1:
+                return False
+
+        return gcd_val >= 2
