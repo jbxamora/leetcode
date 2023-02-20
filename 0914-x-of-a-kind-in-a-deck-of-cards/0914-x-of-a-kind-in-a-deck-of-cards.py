f59b499cb15ff945a4d77e9a6fddce5c69d4fdc7
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-02-20 02:23:09 -0800

    Time: 148 ms (35.48%), Space: 14.3 MB (24.38%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
@@ -1,15 +1,33 @@
-class Solution(object):
-    def hasGroupsSizeX(self, deck):
-        d = {}
+import math
+
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        # Create a frequency dictionary to store the count of each unique card
+        freq = {}
         for x in deck:
-            d[x] = d.get(x, 0) + 1
-        res = set(d.values())
-        if 1 in res: return False
-        if len(res) == 1: return True
-        div = res.pop()
-        for x in res:
-            while x:
-                div, x = x, div % x
-            if div == 1: return False
-        return div != 1
+            freq[x] = freq.get(x, 0) + 1
+        
+        # Get a list of the card counts, sorted in ascending order
+        values = sorted(freq.values())
+        
+        # Check if the smallest count is less than or equal to 1
+        smallest = values[0]
+        if smallest <= 1:
+            return False 
+        
+        # Find the factors of the smallest count
+        factors = set()
+        for i in range(2, int(smallest ** 0.5) + 1):
+            if smallest % i == 0:
+                factors.add(i)
+                factors.add(smallest // i)
+        factors.add(smallest)
+        
+        # Check if the deck can be divided into groups of size X for any factor of the smallest count
+        for f in factors:
+            if all(x % f == 0 for x in values):
+                return True
+        
+        # If the deck cannot be divided into groups of any size, return False
+        return False
 
