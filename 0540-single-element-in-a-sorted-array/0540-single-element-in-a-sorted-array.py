5573db5e462cfa17d7b45f66154108d015e5cef0
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-02-21 22:41:23 -0800

    Time: 264 ms (11.44%), Space: 23.8 MB (38.99%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,31 +1,35 @@
-class Solution(object):
-    def singleNonDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # Use binary search to find the single element
-        left, right = 0, len(nums) - 1
-        while left < right:
-            mid = (left + right) // 2
-            # Check if mid is the single element
-            if mid % 2 == 0 and nums[mid] == nums[mid + 1]:
-                left = mid + 2
-            elif mid % 2 == 1 and nums[mid] == nums[mid - 1]:
-                left = mid + 1
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        # Initialize the left and right indices
+        l = 0
+        h = len(nums) - 1
+        # Loop until the left and right indices converge
+        while l <= h:
+            # Calculate the midpoint index
+            m = (l + h) // 2
+            # Check if the single element is at the end of the list
+            if m == len(nums) - 1:
+                return nums[m]
+            # Check if the single element is to the right of the midpoint
+            if (m % 2 == 1 and nums[m] == nums[m - 1]) or (m % 2 == 0 and nums[m] == nums[m + 1]):
+                l = m + 1
+            # Check if the single element is to the left of the midpoint
             else:
-                right = mid
+                if l == m == h or m % 2 == 1:
+                    h = m - 1
+                else:
+                    h = m
         # Return the single element
-        return nums[left]
+        return nums[m]
     
 # Here's how the solution works:
 
-# The singleNonDuplicate function takes in a sorted list of integers as input and returns the single element that appears only once in the list.
+# The singleNonDuplicate method takes in a list of integers as input and returns the single element that appears only once in the list.
 # The function uses binary search to find the single element in O(log n) time complexity.
-# The left and right pointers are initialized to the first and last indices of the list, respectively.
-# In each iteration of the loop, the mid pointer is set to the middle index between left and right.
-# If mid is even and nums[mid] == nums[mid+1], then the single element must be on the right side of mid. So, we set left = mid + 2.
-# If mid is odd and nums[mid] == nums[mid-1], then the single element must be on the right side of mid. So, we set left = mid + 1.
-# Otherwise, the single element must be on the left side of mid. So, we set right = mid.
-# The loop continues until left == right, at which point we have found the single element.
-# Finally, the function returns the single element.
+# The l and h indices are initialized to the first and last indices of the list, respectively.
+# In each iteration of the loop, the m index is set to the middle index between l and h.
+# If m is at the end of the list, then it must be the single element, so we return it.
+# If nums[m] is equal to the number before or after it, then the single element must be on the right side of m, so we set l = m + 1.
+# Otherwise, the single element must be on the left side of m, so we update h based on whether m is even or odd.
+# We continue this process until l and h converge, at which point we have found the single element.
+# Finally, we return the single element.
