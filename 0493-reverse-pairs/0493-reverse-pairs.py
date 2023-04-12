f769e9709119cfe4400ff859b8c9aa762364a9b4
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-04-12 14:36:05 -0700

    Time: 1272 ms (97.40%), Space: 21.6 MB (47.70%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,44 +1,14 @@
+
+from sortedcontainers import SortedList
+
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        def merge_sort(arr: List[int], start: int, end: int) -> int:
-            if start >= end:
-                return 0
+        sorted_nums = SortedList()
+        count = 0
 
-            mid = start + (end - start) // 2
-            count = merge_sort(arr, start, mid) + merge_sort(arr, mid + 1, end)
+        for num in nums:
+            count += len(sorted_nums) - sorted_nums.bisect_right(2 * num)
+            sorted_nums.add(num)
 
-            # Count important reverse pairs
-            i, j = start, mid + 1
-            while i <= mid and j <= end:
-                if arr[i] > 2 * arr[j]:
-                    count += mid - i + 1
-                    j += 1
-                else:
-                    i += 1
+        return count
 
-            # Merge two sorted halves
-            merged = []
-            i, j = start, mid + 1
-            while i <= mid and j <= end:
-                if arr[i] <= arr[j]:
-                    merged.append(arr[i])
-                    i += 1
-                else:
-                    merged.append(arr[j])
-                    j += 1
-
-            while i <= mid:
-                merged.append(arr[i])
-                i += 1
-
-            while j <= end:
-                merged.append(arr[j])
-                j += 1
-
-            # Update the original array
-            for i in range(len(merged)):
-                arr[start + i] = merged[i]
-
-            return count
-
-        return merge_sort(nums, 0, len(nums) - 1)
